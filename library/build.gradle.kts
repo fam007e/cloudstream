805756d96dce03816dfe86aa8fa0551c9cf68d57
Faisal Ahmed Moshiur <19180457+fam007e@users.noreply.github.com>
2026-04-18 09:43:20 +0600

    docs: finalize fork personalization with verified README and build links

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -131,7 +131,7 @@ dokka {
 
             sourceLink {
                 localDirectory = file("..")
-                remoteUrl("https://github.com/recloudstream/cloudstream/tree/master")
+                remoteUrl("https://github.com/fam007e/cloudstream/tree/master")
                 remoteLineSuffix = "#L"
             }
         }
